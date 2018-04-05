fe68e2bec481d2ddc0b62756166d7c86defce31a
michelleg27 <mgomez48@mail.csuchico.edu>
2018-04-05 12:06:14 -0700

    Fixed

--- main.cpp
+++ main.cpp
@@ -76,49 +76,49 @@ int main()
     {
         winner = board[0][0]; 
         playing = false; 
-        cout<<"Winner is  "<<winner<<endl; 
+        cout<<"Winner is "<<winner<<endl; 
     }
     else if (board[1][0] !=' ' && board[1][0] == board[1][1] && board[1][0] == board[1][2] )
     {
         winner = board[1][0]; 
         playing = false; 
-        cout<<"Winner is  "<<winner<<endl; 
+        cout<<"Winner is "<<winner<<endl; 
     }
     else if (board[2][0] !=' ' && board[2][0] == board[2][1] && board[2][0] == board[2][2] )
     {
         winner = board[2][0]; 
         playing = false; 
-        cout<<"Winner is  "<<winner<<endl; 
+        cout<<"Winner is "<<winner<<endl; 
     }
     else if ( board[0][0] !=' ' && board[0][0] == board[0][1] && board[0][0] == board[0][2])
     {
         winner = board[0][0]; 
         playing = false; 
-        cout<<"Winner is  "<<winner<<endl; 
+        cout<<"Winner is "<<winner<<endl; 
     }
    else if (board[0][1] !=' ' && board[0][1] == board[1][1] && board[0][1] == board[2][1])
    {
         winner = board[0][1]; 
         playing = false; 
-        cout<<"Winner is  "<<winner<<endl; 
+        cout<<"Winner is "<<winner<<endl; 
    }
    else if (board[0][2] !=' ' && board[0][2] == board[1][2] && board[0][2] == board[2][2])
    {
         winner = board[0][2]; 
         playing = false; 
-        cout<<"Winner is  "<<winner<<endl; 
+        cout<<"Winner is "<<winner<<endl; 
    }
    else if (board[0][0] != ' ' && board[0][0] == board[1][1] && board[0][0] == board[2][2])
    {
         winner = board[0][0]; 
         playing = false; 
-        cout<<"Winner is  "<<winner<<endl; 
+        cout<<"Winner is "<<winner<<endl; 
    }
    else if (board[0][2] !=' ' && board[0][2] == board[1][1] && board[0][2] == board[2][0])
    {
         winner = board[0][2]; 
         playing = false; 
-        cout<<"Winner is  "<<winner<<endl; 
+        cout<<"Winner is "<<winner<<endl; 
    }
     //TODO: Print the current board
     //Outline
